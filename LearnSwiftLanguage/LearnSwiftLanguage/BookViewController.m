2d7f692c62767429461cfe2d4dda441e1575b552
wenbobao <744865006@qq.com>
2016-12-12 11:02:13 +0800

    add book source

--- LearnSwiftLanguage/LearnSwiftLanguage/BookViewController.m
+++ LearnSwiftLanguage/LearnSwiftLanguage/BookViewController.m
@@ -6,44 +6,55 @@
 //  Copyright © 2016年 __company__. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "BookViewController.h"
 #import "BookTitleCell.h"
+#import <SafariServices/SafariServices.h>
 
-@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@interface BookViewController ()
 @property (strong, nonatomic) NSMutableArray *dataSource;
 @end
 
-@implementation ViewController
+@implementation BookViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.tableView.delegate = self;
-    self.tableView.dataSource = self;
+    self.title = @"swift基础语法";
     [self.tableView registerNib:[UINib nibWithNibName:@"BookTitleCell" bundle:nil] forCellReuseIdentifier:@"BookTitleCell"];
+    
+    NSData *JSONData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"data" ofType:@"json"]];
+    
+    NSDictionary *data = [NSJSONSerialization JSONObjectWithData:JSONData options:NSJSONReadingAllowFragments error:nil];
+    
+    self.dataSource = data[@"swift"];
+    
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 3;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return self.dataSource.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     BookTitleCell *cell = [tableView dequeueReusableCellWithIdentifier:@"BookTitleCell" forIndexPath:indexPath];
-    cell.name.text = @"Good";
+    cell.name.text = self.dataSource[indexPath.row][@"name"];
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSURL *url = [NSURL URLWithString:self.dataSource[indexPath.row][@"url"]];
+    
+    [self presentViewController:[[SFSafariViewController alloc]initWithURL:url] animated:YES completion:NULL];
+    
 }
 
 @end
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SwiftProgrammingLanguage
-Learn the Swift 3.0 Programming language 
+
+Learn the Swift 3.0 Programming language simple APP
+
+- [https://www.cnswift.org/](https://www.cnswift.org/)
